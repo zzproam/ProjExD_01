3ef127db1da11515b693c4852df181ecedf572e1
Am <c0a2210531@edu.teu.ac.jp>
2023-11-21 16:53:27 +0900

    演習

--- pygame_image.py
+++ pygame_image.py
@@ -1,27 +1,36 @@
 import sys
 import pygame as pg
-
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((900, 600))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    bg_img_2 = bg_img
-    bird = pg.image.load("ex01/fig/3.png")
-    bird = pg.transform.flip(bird,True,False)
-    bird = [bird,pg.transform.rotozoom(bird, 10, 1.0)]
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")  # 練習１：背景画像Surfaceの生成
+    bg_img_2=pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("ex01/fig/3.png")  # 練習２：こうかとん画像Surfaceの生成
+    kk_img = pg.transform.flip(kk_img, True, False)  # 練習２：こうかとんの左右反転
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]  # 練習３：こうかとんSurfaceのリスト
     tmr = 0
+    bird_deg=0
+    count=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%1600
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img_2, [1600-x, 0])
-        screen.blit(bird[tmr%2],[300,200])
+
+        x = tmr%1600  # 練習６：動く背景画像
+        screen.blit(bg_img, [-x, 0])  # 練習４：背景画像の表示
+        screen.blit(bg_img_2, [1600-x, 0])  # 練習６：動く背景画像
+        screen.blit(kk_imgs[bird_deg], [300, 200])  # 練習５：こうかとんはばたく
+        print(-x,1600-x,bird_deg)
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+        count+=1
+        if bird_deg==0 and count%20==0:
+            bird_deg=1
+        elif bird_deg==1 and count%20==0:
+            bird_deg=0
         clock.tick(100)
+        
+
 
 if __name__ == "__main__":
     pg.init()
